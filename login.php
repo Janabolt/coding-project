10dd834d9739364ef10966acb83c31b52a1d8013
Jana <janamaran42@gmail.com>
2017-08-16 16:37:44 +0800

    Update and rename README.md to login.php

---
+++ login.php
@@ -0,0 +1,107 @@
+<!DOCTYPE html> 
+<html>
+
+ <head>
+
+     <title>jtc</title>
+
+     </head>
+
+
+     <body>
+         
+          <h1>JTC</h1>
+          
+          <P>All rights reserved @ Joker And Cooperation</P>
+
+
+          <a href="index.php">Click here to exit to the previous page</a></br></br>
+
+          <form action="checklogin.php" method="POST">
+              ENTER USERNAME: <input type="text" name="username" required="required"/> </br>
+              ENTER PASSWORD: <input type="password" name="password" required="required"/> </br>
+              <input type="submit" value="Login"/>
+           </form>
+      </body>
+
+ <body>
+
+<p2>For more information please call our customer care at +60 145267645 OR email us jtc@gmail.com<p2>
+
+<style>
+
+   table { 
+      border-collapse: collapse;
+
+    }
+
+    table, td, th {
+           border: 5px solid black;
+         }
+
+
+body {
+  background-color: white;
+
+}
+
+p {  text-align: center;
+}
+ 
+h1 { 
+
+     color: black;
+     text-align: center;
+ }
+
+ }
+
+ p  {  
+       font-family: verdana;
+       font-size:20px;
+
+
+    }
+a    {  
+       color: black;
+
+
+     }
+
+ form { 
+        color: black;
+        text-align: center;
+
+
+       }     
+
+input {
+
+       color: blue; 
+}
+ 
+
+    </style>
+
+<table>
+<tr>
+   <th>Language name</th>
+   <th>Price</th>
+
+   </tr>
+   <tr>
+   <td>HTML</td>
+   <td>RM50 per class</td>
+   </tr>
+
+   <tr>
+   <td>CSS</td>
+   <td>RM60per class</td>
+   </tr>
+
+   </table>
+
+   <p><b>Note:</b> If you're really interested to join our codding club,you can register first and then you'll receive an email from our company.</p>
+
+   </body>
+   </html>
